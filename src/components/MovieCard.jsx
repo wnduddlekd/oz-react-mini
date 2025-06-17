363e3ad92675395ec3a2d007cf038a8a5c223cf1
wndud <ju03209@naver.com>
2025-06-17 16:00:12 +0900

    style: MovieDetail 전체적인 UI 변경

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,9 +1,11 @@
 export default function MovieCard({ movie }) {
   return (
-    <div key={movie.id} className="w-64 h-100 border border-gray-300">
-      <img src={movie.poster} />
-      <h3 className="p-2 font-semibold">{movie.title}</h3>
-      <p className=" p-1 text-sm text-gray-600">평점: {movie.score}</p>
+    <div key={movie.id} className="w-64 border border-gray-300 overflow-hidden">
+      <img src={movie.poster} className="w-full h-[370px] object-cover" />
+      <div className="p-1">
+        <h3 className="m-2 font-semibold">{movie.title}</h3>
+        <p className=" m-2 text-sm text-gray-600">평점: {movie.score}</p>
+      </div>
     </div>
   );
 }

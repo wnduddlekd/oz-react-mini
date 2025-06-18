c13e4a798a6c85ab27b6be4b261c67df4042f5c3
wndud <ju03209@naver.com>
2025-06-18 18:27:49 +0900

    feat: 전체적인 UI 변경

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -10,12 +10,17 @@ export default function MovieCard({ movie }) {
     <div
       onClick={handleClick}
       key={movie.id}
-      className="w-64 border border-gray-300 overflow-hidden"
+      className="w-64 overflow-hidden rounded bg-slate-950/20 shadow-xl shadow-gray-700/40 transition-transform duration-300 ease-in-out hover:-translate-y-2 mb-2 p-2"
     >
-      <img src={movie.poster} className="w-full h-[370px] object-cover" />
+      <img
+        src={movie.poster}
+        className="w-full h-[370px] object-cover rounded"
+      />
       <div className="p-1">
-        <h3 className="m-2 font-semibold">{movie.title}</h3>
-        <p className=" m-2 text-sm text-gray-600">평점: {movie.score}</p>
+        <h3 className="m-2">{movie.title}</h3>
+        <p className=" m-2 text-sm text-gray-500">
+          평점: {movie.score.toFixed(2)}
+        </p>
       </div>
     </div>
   );
--- src/pages/NavBar.jsx
+++ src/pages/NavBar.jsx
@@ -1,9 +1,14 @@
 export default function NavBar() {
   return (
-    <div className="flex items-center justify-between px-6 p-4 w-[800px]">
-      <h1 className="w-32 text-3xl font-semibold">OZ무비.</h1>
-      <div className="border border-gray-300 rounded px-3 py-1 ">
-        <input type="text" className="focus:outline-none w-[450px]" />
+    <div className="flex items-center justify-between px-12 p-6 w-full bg-gray-950">
+      <div className="flex">
+        <h1 className="w-32 text-3xl font-semibold">OZ무비.</h1>
+        <div className="hidden lg:block border border-gray-300/30 rounded px-3 py-2">
+          <input
+            type="text"
+            className="focus:outline-none w-[450px] bg-transparent caret-white"
+          />
+        </div>
       </div>
       <div className="flex gap-4">
         <button>로그인</button>

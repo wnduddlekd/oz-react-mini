22b4be6ae63e3706c732c932b105032dbebf0441
wndud <ju03209@naver.com>
2025-06-20 13:19:35 +0900

    feat: 검색 상태를 체크하는 SearchContext 컴포넌트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,19 @@ import { Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import MovieDetail from './pages/MovieDetail';
+import { SearchProvider } from './components/SearchContext';
 
 function App() {
   return (
     <div className="flex flex-col items-center">
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="detail/:id" element={<MovieDetail />} />
-        </Route>
-      </Routes>
+      <SearchProvider>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="detail/:id" element={<MovieDetail />} />
+          </Route>
+        </Routes>
+      </SearchProvider>
     </div>
   );
 }

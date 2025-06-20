569c5d4677ef6021490f8a9069f81bf47da20264
wndud <ju03209@naver.com>
2025-06-20 13:31:56 +0900

    feat: MovieList에 검색 결과 반영

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,11 +1,24 @@
 import MovieCard from './MovieCard';
+import { useSearch } from './SearchContext';
 
 export default function MovieList({ movies }) {
+  const { searchTerm } = useSearch();
+
+  const filterMovie = searchTerm
+    ? movies.filter((movie) => movie.title.includes(searchTerm))
+    : movies;
+
   return (
-    <div className="flex flex-wrap justify-center gap-6 py-12 ">
-      {movies.map((movie) => (
-        <MovieCard key={movie.id} movie={movie} />
-      ))}
+    <div>
+      {filterMovie.length === 0 ? (
+        <p className="my-64">검색 결과가 없습니다</p>
+      ) : (
+        <div className="flex flex-wrap justify-center gap-6 py-12 ">
+          {filterMovie.map((movie) => (
+            <MovieCard key={movie.id} movie={movie} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }

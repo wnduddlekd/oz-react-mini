d52a968e1be7a16d2434a7d7d54e26df3b6b23ee
wndud <ju03209@naver.com>
2025-06-20 16:52:48 +0900

    feat: 디테일 페이지 UI 수정

--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -20,7 +20,6 @@ export default function MovieDetail() {
           genres: data.genres,
           overview: data.overview,
           runtime: data.runtime,
-          release_date: data.release_date,
           tagline: data.tagline,
           homepage: data.homepage,
         };
@@ -33,18 +32,40 @@ export default function MovieDetail() {
   if (!detail || !detail.poster)
     return <p className="mt-5">detail 페이지 불러오는중 . . .</p>;
   return (
-    <div
-      key={detail.id}
-      className="flex mt-16 p-4 border border-gray-300 w-[800px]"
-    >
-      <img src={detail.poster} className="w-96" />
-      <div className="flex flex-col gap-4 p-8 w-96">
-        <div className="flex justify-between">
-          <h2 className="text-xl font-semibold">{detail.title}</h2>
-          <p className=" p-1 text-sm text-gray-600">평점: {detail.score}</p>
+    <div className="w-full">
+      <div className="overflow-hidden">
+        <img src={detail.backdrop} className="w-full opacity-20 object-cover" />
+      </div>
+      <div
+        key={detail.id}
+        className="flex fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 border border-gray-300 w-[1000px] rounded-lg bg-slate-800/50 shadow-xl shadow-gray-700/40"
+      >
+        <img src={detail.poster} className="w-96" />
+        <div className="flex flex-col gap-4 p-8">
+          {detail.tagline && (
+            <p className="italic text-red-300 text-xl">" {detail.tagline} "</p>
+          )}
+          <div>
+            <div className="flex justify-between">
+              <h2 className="text-xl font-semibold">{detail.title}</h2>
+              <p className="p-1 text-sm text-gray-600">평점: {detail.score}</p>
+            </div>
+            <p className="text-sm text-gray-500">
+              상영시간: {detail.runtime} 분
+            </p>
+          </div>
+          <ul className="flex gap-2">
+            {detail.genres.map((genre) => (
+              <li
+                key={genre.id}
+                className="py-1 px-2 rounded-xl text-white bg-cyan-800"
+              >
+                {genre.name}
+              </li>
+            ))}
+          </ul>
+          <p className="leading-relaxed">{detail.overview}</p>
         </div>
-        <h3>{detail.genres.map((genre) => genre.name).join(', ')}</h3>
-        <p className="leading-relaxed">{detail.overview}</p>
       </div>
     </div>
   );
